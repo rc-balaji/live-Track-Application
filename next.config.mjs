1a01407637b9ff40ce5550ab86a3806f18c3d76e
rc-balaji <rcbalaji2003@gmail.com>
2025-02-27 13:10:35 +0530

    New1

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@": require("path").resolve(__dirname), // Alias "@" to the root directory
+      "@": path.resolve(__dirname), // Alias "@" to the root directory
     };
     return config;
   },
